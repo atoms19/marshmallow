0403676ecbf55cbdbedeed4e1828f14825080039
saxophone-dev <me@saxophone.is-a.dev>
2025-03-11 11:34:22 +0530

    font

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,8 +1,20 @@
+import { createSignal } from "solid-js";
+import { toast } from "../components/toast";
+import Button from "../components/button";
+
 const Index = () => {
+  const [count, setCount] = createSignal(0);
+
+  const incrementCount = () => {
+    setCount(count() + 1);
+    toast(`Count: ${count()}`, "success");
+  };
+
   return (
-    <>
-      <h1>Index</h1>
-    </>
+    <div class="flex items-center justify-center h-screen flex-col">
+      <h1 class="mb-4 text-3xl font-bold">Welcome to Dango!</h1>
+      <Button onClick={incrementCount}>Count?</Button>
+    </div>
   );
 };
 
