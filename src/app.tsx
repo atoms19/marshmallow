dd3368fe047441e3ebb540f4931c7bd6931246b7
saxophone-dev <me@saxophone.is-a.dev>
2025-03-11 10:56:17 +0530

    add font, scaffold routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,22 @@
 import "./app.css";
+import "@fontsource-variable/hanken-grotesk/wght.css";
+import "@fontsource-variable/hanken-grotesk/wght-italic.css";
+
+import { lazy } from "solid-js";
+
+const Index = lazy(() => import("./routes/_index"));
+const Auth = lazy(() => import("./routes/auth"));
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Route path="/" component={Index} />
+        <Route path="/auth" component={Auth} />
+      </Router>
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
